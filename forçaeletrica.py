dc258ba88dca48e8db9d7ab00da96447c339c9e3
Gabriel Holanda <139644915+DutchRagazzo@users.noreply.github.com>
2025-02-23 16:28:01 -0300

    Add files via upload

---
+++ forçaeletrica.py
@@ -0,0 +1,41 @@
+#tive uma pequena ajuda do ChatGPT para descobrir meus erros em relação a multiplicação de Q e q pelos prefixos, futuramente irei adicionar a funcionalidade de converter as medidas de distancia caso não estiverem em metros.
+from math import pow
+
+print('Bem-vindo à calculadora de Força Elétrica!')
+
+k = 9 * pow(10, 9)  # Constante de Coulomb
+
+Q = float(input('Insira a carga elétrica de Q: '))
+q = float(input('Insira a carga elétrica de q: '))
+
+# Criando variáveis para os prefixos das cargas
+mC = pow(10, -6)  # Microcoulomb
+nC = pow(10, -9)  # Nanocoulomb
+miliC = pow(10, -12)  # Milicoulomb
+
+# Descobrindo o prefixo das cargas
+askprefixo = input('Em qual prefixo estão as cargas? (Responda micro, nano ou mili): ').lower() #CONVERTE O QUE FOI DIGITADO EM MINUSCULAS
+
+if askprefixo == 'micro':
+    Q *= mC ##atualiza o valor de Q e q pela multiplicação deles pelo mC
+    q *= mC
+elif askprefixo == 'mili':
+    Q *= miliC ##atualiza o valor de Q e q pela multiplicação deles pelo miliC
+    q *= miliC
+elif askprefixo == 'nano':
+    Q *= nC ##atualiza o valor de Q e q pela multiplicação deles pelo nanoC
+    q *= nC
+else:
+    print("Prefixo não reconhecido. Usando o valor original das cargas.")
+    
+d = float(input('Insira a distância entre os corpos em metros: '))
+
+# Determinando a natureza da força (atração ou repulsão)
+Fnature = 'repulsão' if Q == q else 'atração'
+
+# Calculando a força (utilizando abs() para garantir valores positivos)
+F = k * abs(Q) * abs(q) / pow(d, 2)
+
+# Exibindo o resultado com no máximo duas casas decimais
+print(f'A intensidade da força de {Fnature} entre os corpos é de: {F:.2f} N')
+
