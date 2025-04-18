f0ea933f817180c979eb2107fb7227299af7e2e9
Gabriel Holanda <139644915+DutchRagazzo@users.noreply.github.com>
2025-04-18 18:52:07 -0300

    Update forçaeletrica.py

--- forçaeletrica.py
+++ forçaeletrica.py
@@ -9,9 +9,9 @@ Q = float(input('Insira a carga elétrica de Q: '))
 q = float(input('Insira a carga elétrica de q: '))
 
 # Criando variáveis para os prefixos das cargas
-mC = pow(10, -6)  # Microcoulomb
-nC = pow(10, -9)  # Nanocoulomb
-miliC = pow(10, -3)  # Milicoulomb
+microColoumb = pow(10, -6)  # Microcoulomb
+nanoColoumb = pow(10, -9)  # Nanocoulomb
+miliColoumb = pow(10, -3)  # Milicoulomb
 
 # Descobrindo o prefixo das cargas
 askprefixo = input('Em qual prefixo estão as cargas? (Responda micro, nano ou mili): ').lower() #CONVERTE O QUE FOI DIGITADO EM MINUSCULAS
@@ -27,14 +27,24 @@ elif askprefixo == 'nano':
     q *= nC
 else:
     print("Prefixo não reconhecido. Usando o valor original das cargas.")
-    
-d = float(input('Insira a distância entre os corpos em metros: '))
 
+distancia = float(input('Insira a distância entre os corpos: '))
+
+#variáveis para converter outras medidas em metro
+centimetros = pow(10, -1)
+milimetros = pow(10, -3)
+    
+askdistancia = str(input('Em qual medida está a distância ( Responda metros (m), centimentros(cm), milimetros(mm): ')).lower()
+
+if askdistancia == 'centimetros' or askdistancia == 'cm'
+distancia *= centimetros
+if askdistancia == 'miliimetros' or askdistancia == 'mm'
+distancia *= milimetros
 # Determinando a natureza da força (atração ou repulsão)
 Fnature = 'repulsão' if Q == q else 'atração'
 
 # Calculando a força (utilizando abs() para garantir valores positivos)
-F = k * abs(Q) * abs(q) / pow(d, 2)
+F = k * abs(Q) * abs(q) / pow(distancia, 2)
 
 # Exibindo o resultado com no máximo duas casas decimais
 print(f'A intensidade da força de {Fnature} entre os corpos é de: {F:.2f} N')
